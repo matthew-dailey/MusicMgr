8b02396e9d89d8254902aa51d9696335dc4fb44c
Matt Dailey <matthew.dailey1@gmail.com>
2014-06-28 10:31:34 -0400

    Added in Events collection to MusicMgr.js, added templates to dashboard for displaying events.

--- client/js/MusicMgr.js
+++ client/js/MusicMgr.js
@@ -1,5 +1,28 @@
+Events = new Meteor.Collection("events");
+
 Template.dashboard.user = function() {
     var user = Meteor.user();
     if (user && user.emails)
         return user.emails[0].address;
-}
+};
+
+Template.dashboard.hasevents = function() {
+    var user = Meteor.user();
+    if (user && user.emails) {
+        var email = user.emails[0].address;
+        return Events.find({user: email}, {sort: {name: 1}}).count() > 0;
+    } else {
+        return false;
+    }
+};
+
+Template.dashboard.events = function() {
+    var user = Meteor.user();
+    if (user && user.emails) {
+        var email = user.emails[0].address;
+        return Events.find({user: email}, {sort: {name: 1}});
+    } else {
+        //return new Array("No events!");
+        return null;
+    }
+};
