ccee7091776e418059a8db13ccbc5f193afdea4d
Matt Dailey <matthew.dailey1@gmail.com>
2014-07-13 20:54:15 -0400

    Properly set up 'event' template in dashboard.html and the functions to populate it in MusicMgr.js.
    - Also added startup function on server to create test data if no event data exists.

--- client/js/MusicMgr.js
+++ client/js/MusicMgr.js
@@ -4,30 +4,42 @@ Meteor.subscribe("events");
 // don't declare Events here; this sources model.js which already declared it
 //Events = new Meteor.Collection("events");
 
+// Returns the Collection cursor for events for a user if user found.
+// Null if no user/email found
+var getEvents = function(user) {
+    var userEvents = null;
+    console.log("getEvents");
+    if (user && user.emails) {
+        var email = user.emails[0].address;
+        //var userEvents = Events.find({user: email}, {sort: {name: 1}});
+        console.log("getEvents: valid user '" + email + "'");
+        userEvents = Events.find({});
+        console.log("getEvents: events: '" + userEvents.fetch()[0].name + "'");
+    }
+    return userEvents;
+}
+
+// return current user's email
 Template.dashboard.user = function() {
     var user = Meteor.user();
     if (user && user.emails)
         return user.emails[0].address;
 };
 
+// return true if the user exists and has events
 Template.dashboard.hasevents = function() {
     var user = Meteor.user();
-    if (user && user.emails) {
-        var email = user.emails[0].address;
-        var userEvents = Events.find({user: email}, {sort: {name: 1}});
-        return userEvents.count() > 0;
-    } else {
-        return false;
-    }
+    var userEvents = getEvents(user);
+    return (userEvents && userEvents.count() > 0);
 };
 
-Template.dashboard.events = function() {
+// return a cursor over all events for this user
+Template.dashboard.userevents = function() {
     var user = Meteor.user();
-    if (user && user.emails) {
-        var email = user.emails[0].address;
-        //return Events.find({user: email}, {sort: {name: 1}});
-        return Events.find({});
-    } else {
-        return null;
-    }
+    return getEvents(user);
+};
+
+// return name of an event
+Template.event.name = function() {
+    return this.name;
 };
--- server/serve.js
+++ server/serve.js
@@ -1,5 +1,16 @@
 // don't declare Events here; this sources model.js which already declared it
 //Events = new Meteor.Collection("events");
+
+Meteor.startup(function() {
+    // add test data if none exists
+    var cursor = Events.find();
+    if (cursor.count() === 0) {
+        console.log("No events found; adding dummy events.");
+        Events.insert({name: "Cool Event"});
+        Events.insert({name: "UnCool Event"});
+    }
+});
+
 Meteor.publish("events", function() {
     // pull back all events; later filter by user
     return Events.find({});
